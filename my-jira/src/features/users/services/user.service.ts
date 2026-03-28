a6d045e0254c8e13ce86df9fb8519fab0b2a7b68
mihuwis <mihuwis@gmail.com>
2026-03-28 10:37:48 +0100

    Add list of users

--- my-jira/src/features/users/services/user.service.ts
+++ my-jira/src/features/users/services/user.service.ts
@@ -3,9 +3,20 @@ import type { User } from "../types/user.types";
 const MOCK_USER_1: User = {
     id:"123", firstName:"Jan", lastName:"Nowak"
 }
+const MOCK_USER_2: User = {
+    id:"125", firstName:"Sybilla", lastName:"Kociupko"
+}
+
+const LIST_OF_MOCK_USERS : User[] =
+    [MOCK_USER_1, MOCK_USER_2]
+
 
 const getCurrentUser = () : User => {
     return MOCK_USER_1;
+}
+
+const getAllUsers = () : User[] => {
+    return LIST_OF_MOCK_USERS;
 }
 
 export const userService = {getCurrentUser}
