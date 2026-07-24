de7f8eb12e7fbf28c619fc39642ff88db683ea80
mihuwis <mihuwis@gmail.com>
2026-07-24 08:42:03 +0200

    Add menu elements

--- my-jira/src/app/layout/AppLayout.tsx
+++ my-jira/src/app/layout/AppLayout.tsx
@@ -1,20 +1,22 @@
 import type { ReactNode } from 'react';
+import { userService } from '../../features/users/services/user.service';
 
 
 type AppLayoutProps = {
   children: ReactNode;
 };
 
+var currentUser : User = userService.getCurrentUser();
+
 
 export function AppLayout({ children }: AppLayoutProps){
     return (
         <div>
             <header>
-                Mini-Jira <a href="/projects">Projects</a>
+                Mini-Jira | <a href="/projects">Projects</a> | Hello { currentUser.firstName } your role is { currentUser.role }
             </header>
                 {children}
         </div>
-
 
     )
 }
