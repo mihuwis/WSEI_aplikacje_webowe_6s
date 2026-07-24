c79b301c44bb02ed307cec86495528d3a4904a98
mihuwis <mihuwis@gmail.com>
2026-07-24 09:54:58 +0200

    Add missing project info

--- my-jira/src/features/tasks/pages/TasksBoardPage.tsx
+++ my-jira/src/features/tasks/pages/TasksBoardPage.tsx
@@ -4,6 +4,10 @@ import { useParams } from "react-router-dom";
 export function TasksBoardPage() {
 
   const params = useParams();
+
+  if (!params.projectId) {
+    return <div>Project ID is missing in the URL.</div>;
+  }
   return (
     <div>
       <h1>Tasks Board for {params.projectId}</h1>
